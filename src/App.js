283ab70f7bafc8e977afe7fb64cf0590bd87ee13
Roshroy <roshroygeorge@gmail.com>
2023-06-15 20:18:47 +0530

    initial change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://2d97-2405-201-f001-a1c4-5ca9-ec26-1d1a-ac01.ngrok-free.app/users/sampleview/');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const postData = async () => {
+    try {
+      const response = await fetch('https://2d97-2405-201-f001-a1c4-5ca9-ec26-1d1a-ac01.ngrok-free.app/users/sampleview/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ key: 'value' }),
+      });
+      const responseData = await response.json();
+      console.log('Response from backend:', responseData);
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Data Details</h2>
+      <p>{data}</p>
+      <button onClick={postData}>Post Data</button>
+
     </div>
   );
 }
